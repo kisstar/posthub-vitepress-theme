e26f8ecddf7f70083d8d7824be7e9c0b28f24b13
kisstar <dwh.chn@foxmail.com>
2023-11-26 15:39:12 +0800

    feat(theme): add meta information only for blog posts

--- packages/posthub-vitepress-theme/src/components/layout/PHLayout.vue
+++ packages/posthub-vitepress-theme/src/components/layout/PHLayout.vue
@@ -1,16 +1,21 @@
 <script setup>
 import { useData } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import { DEFAULT_POSTS_DIRNAMES } from '../../config';
 import PHPostMeta from './components/PHPostMeta.vue';
 
-const { frontmatter } = useData();
+const { frontmatter, page } = useData();
+
+const isPostPage = DEFAULT_POSTS_DIRNAMES.some((dirname) =>
+  page.value.filePath.startsWith(`${dirname}/`)
+);
 
 const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
-    <template #doc-before>
+    <template v-if="isPostPage" #doc-before>
       <PHPostMeta :post-meta="frontmatter"></PHPostMeta>
     </template>
   </Layout>
--- packages/posthub-vitepress-theme/src/config/index.ts
+++ packages/posthub-vitepress-theme/src/config/index.ts
@@ -1,2 +1,5 @@
 // 记录最近浏览的标签数量
 export const MAX_RECENT_TAGS = 20;
+
+// 默认的博文所在目录名
+export const DEFAULT_POSTS_DIRNAMES = ['posts'];
