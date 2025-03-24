6d0d555ca94f2f636bb61fe6089c97bb55b3678f
Kisstar <dwh.chn@foxmail.com>
2025-03-24 15:49:27 +0800

    feat(theme): the user information module supports SVG icon configuration

--- packages/posthub-vitepress-theme/src/components/icons/PHIcon.vue
+++ packages/posthub-vitepress-theme/src/components/icons/PHIcon.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { h } from 'vue';
+import { defineComponent, h } from 'vue';
 import PHGitHub from './PHGitHub.vue';
 import PHLocation from './PHLocation.vue';
 import PHJueJin from './PHJueJin.vue';
@@ -18,12 +18,30 @@ const components = {
   tag: PHTag,
 };
 
-export default {
+export default defineComponent({
   props: {
     type: String,
   },
   setup(props) {
-    return () => h(components[props.type || '']);
+    const comp = components[props.type || ''];
+
+    return () => {
+      if (comp) {
+        // 如果组件存在，渲染该组件
+        return h(comp);
+      } else if (props.type?.startsWith('http')) {
+        // 如果 type 以 http 开头，渲染一个图片
+        return h('img', { src: props.type });
+      } else if (props.type) {
+        // 如果 type 有值但组件不存在且不以 http 开头，渲染一个 svg 元素
+        return h('div', {
+          innerHTML: props.type,
+        });
+      } else {
+        // 如果 type 没有值，不渲染任何内容
+        return null;
+      }
+    };
   },
-};
+});
 </script>
